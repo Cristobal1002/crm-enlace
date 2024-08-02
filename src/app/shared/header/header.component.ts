47c1b5a430ebf1910460b27f0ea21b54dd50f0b9
Cristobal1002 <cristobal1002@gmail.com>
2024-08-01 23:56:45 -0500

    Menu para mobile

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Router, NavigationEnd, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { Menu, MenuItem } from '../../interfaces/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterOutlet, RouterModule, HttpClientModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  menuItems: MenuItem[] = [];
+  isExpandedAdmin = false;
+  isExpandedDonations = false;
+  userRole: string = 'admin'; // Cambia esto según el rol del usuario
+  asideMenu = false
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadMenu();
+    this.checkIfSubmenuShouldBeExpanded(this.router.url);
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.checkIfSubmenuShouldBeExpanded(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  loadMenu() {
+    this.http.get<Menu>('/assets/menu.json').subscribe(data => {
+      this.menuItems = this.filterMenuByRole(data.menu);
+    });
+  }
+
+  filterMenuByRole(menu: MenuItem[]): MenuItem[] {
+    return menu.filter(item => item.roles.includes(this.userRole)).map(item => {
+      if (item.submenu) {
+        item.submenu = this.filterMenuByRole(item.submenu);
+      }
+      return item;
+    });
+  }
+
+  toggleAdminMenu() {
+    this.isExpandedAdmin = !this.isExpandedAdmin;
+  }
+
+  toggleDonationsMenu() {
+    this.isExpandedDonations = !this.isExpandedDonations;
+  }
+
+  private checkIfSubmenuShouldBeExpanded(url: string): void {
+    this.isExpandedAdmin = url.startsWith('/administracion');
+    this.isExpandedDonations = url.startsWith('/donaciones');
+  }
+
+  setAside(){
+    !this.asideMenu
+  }
 
 }
