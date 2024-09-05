6872b26ebb2f50c65d3988e358ef19233dfc7855
Cristobal1002 <cristobal1002@gmail.com>
2024-09-04 19:11:37 -0500

    adecuaciones para despliegue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://my-prod-url'
-};
+    apiUrl: 'http://backend:3100'  // Nombre del servicio y puerto del backend en el contenedor
+  };
