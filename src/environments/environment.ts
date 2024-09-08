e783964cf4510d0c6aa3b47b1a599308f4fd015b
Cristobal1002 <cristobal1002@gmail.com>
2024-09-07 22:01:11 -0500

    Cambio en ruta de prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://backend:3100'  // Nombre del servicio y puerto del backend en el contenedor
+    apiUrl: '/api/'  // Nombre del servicio y puerto del backend en el contenedor
   };
